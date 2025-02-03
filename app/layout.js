f5858cbfe11deea14c98910d32c10118bc46bc5a
phenyo-code <phenyo.koikoi@icloud.com>
2025-02-03 13:49:25 +0200

    meta stuff

--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,28 @@
 
 import "./globals.css"; 
 import { SessionProvider } from "next-auth/react";
-
-
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Global Open Graph Meta Tags */}
+        <meta property="og:title" content="FLARE | Shop Latest Trends In Fashion" />
+        <meta property="og:description" content="Discover the latest fashion trends and shop with confidence." />
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://flare-shop.vercel.app/" />
+        <meta property="og:site_name" content="Your Website Name" />
+        
+        {/* Optionally, Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/opengraph-image.png" />
+      </head>
       <body>
-         <SessionProvider>
-        {children}
-      </SessionProvider>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
